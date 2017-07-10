4a76e84adba63b5e9aa1b8a01adc8d060b91d1f3
donkeypagoda <christopherallenstewart@gmail.com>
2017-07-10 14:18:26 -0700

    got the math for the crossfade worked out

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,3 @@
-// event handlers
-
-let mousePoint = 0;
-let faderValue = 5;
-
-$("#xfader").on("input", function(event){
-  console.log($("#xfader").val());
-})
-
-// listener for the mouseover method
-// $("#fader").on("mousemove", function(event){
-//   console.log(event.offsetX);
-//   mousePoint = event.clientX;
-// });
-
 // the Web Audio Stuff
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 const ch0 = document.querySelector("#ch0")
@@ -23,3 +8,26 @@ const source1 = audioCtx.createMediaElementSource(ch1);
 
 const gain0 = audioCtx.createGain();
 const gain1 = audioCtx.createGain();
+
+let mousePoint = 0;
+let faderValue = 5;
+
+function faderMath(faderValue){
+  let gainL = Math.abs(faderValue - 10);
+  console.log(gainL);
+  let gainR = faderValue;
+  console.log(gainR);
+}
+
+// event handlers
+$("#xfader").on("input", function(event){
+  faderValue = $("#xfader").val();
+  // console.log(faderValue);
+  faderMath(faderValue);
+})
+
+// listener for the mouseover method
+// $("#fader").on("mousemove", function(event){
+//   console.log(event.offsetX);
+//   mousePoint = event.clientX;
+// });
