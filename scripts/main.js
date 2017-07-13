9bcc73b623a9fe72fbe62c42fc01e1884cdfc476
donkeypagoda <christopherallenstewart@gmail.com>
2017-07-12 18:56:17 -0700

    working on layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,9 @@
 // the Web Audio Stuff
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-const ch0 = document.querySelector("#ch0");
-const ch1 = document.querySelector("#ch1");
-
-// local media
-// const source0 = audioCtx.createMediaElementSource(ch0);
-// const source1 = audioCtx.createMediaElementSource(ch1);
 
 // webmedia
 const webSource0 = audioCtx.createBufferSource(ch0);
 const webSource1 = audioCtx.createBufferSource(ch1);
-
-const gain0 = audioCtx.createGain();
-const gain1 = audioCtx.createGain();
-const tuna = new Tuna(audioCtx);
-
-const delay = new tuna.Delay({
-    feedback: 0.45,    //0 to 1+
-    delayTime: 750,    //1 to 10000 milliseconds
-    wetLevel: 0.85,    //0 to 1+
-    dryLevel: 0.2,       //0 to 1+
-    cutoff: 2000,      //cutoff frequency of the built in lowpass-filter. 20 to 22050
-    bypass: 0
-});
-
-// source0.connect(gain0);
-webSource0.connect(delay);
-// gain0input.connect(delay);
-delay.connect(gain0);
-gain0.connect(audioCtx.destination);
-
-// source1.connect(gain1);
-webSource1.connect(gain1);
-gain1.connect(audioCtx.destination);
-
 
 // INTERFACE BULLSHIT
 let faderValue = 0.5;
@@ -49,10 +19,6 @@ $("#xfader").on("input", function(event){
   faderMath(faderValue);
 });
 
-$("#delay").on("input", function(event){
-  delayValue = $("#delay").val();
-});
-
 
 // THIS IS THE NON-OAUTH METHOD FOR 30SECOND SAMPLES FROM NAPSTER
 function getTop(){
@@ -63,6 +29,7 @@ function getTop(){
     if (xhr.status !== 200) {
       return;
     }
+
     let source0 = $("<source>")
     source0.prop("src", data.tracks[1].previewURL);
     source0.prop("type", "audio/mpeg");
@@ -71,8 +38,27 @@ function getTop(){
     source1.prop("src", data.tracks[2].previewURL);
     source1.prop("type", "audio/mpeg");
     $("#ch1").append(source1);
+    // console.log(data);
 
+    // making lists of track choices
+    let list0 = $("<ul>");
+    for (let i = 0; i < data.tracks.length; i++){
+      let listItem0 = $("<li>").text(data.tracks[i].name);
+      let track0 = $("<source>").prop("src", data.tracks[i].previewURL);
+      listItem0.append(track0);
+      list0.append(listItem0);
+      $("#leftList").append(list0);
+    }
+
+    let list1 = $("<ul>");
+    for (let j = 0; j < data.tracks.length; j++){
+      let listItem1 = $("<li>").text(data.tracks[j].name);
+      let track1 = $("<source>").prop("src", data.tracks[j].previewURL);
+      listItem1.append(track1);
+      list1.append(listItem1);
+      $("#rightList").append(list1);
+    }
 
   });
-};
+}
 getTop();
